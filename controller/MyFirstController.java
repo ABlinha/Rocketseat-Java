334dc21c1c5d6929b12dcd8271029ce8e07df4b1
Isabella C <bellacap98@gmail.com>
2023-10-13 17:45:40 -0300

    Dockerfile deploy

---
+++ controller/MyFirstController.java
@@ -0,0 +1,29 @@
+/*NOT TO USE
+
+package com.example.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //criação de uma rota
+@RequestMapping("/firstPath")
+//http://localhost:8080/firstPath/
+public class MyFirstController {
+    
+    /**
+     * Métodos de aceso de uma requisição do HTTP
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/info
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte da info/dado
+     *
+    // Método (funcionalidade) de uma classe
+    @GetMapping("/fisrtMethod")
+    public String firstMessage(){
+        return "Funcionou";
+    }
+}
+*/
